d206e1dab0e4fd13e77b1a24563615ca558ae5ee
Didac19 <diegocastrog19@gmail.com>
2024-09-06 10:19:16 -0500

    improve metadata for SEO

--- app/layout.js
+++ app/layout.js
@@ -8,9 +8,32 @@ const preavihear = Preahvihear({ subsets: ['khmer'], weight: '400' })
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'DCG',
-  description: 'Web developer portfolio',
-}
+  title: 'DidacDev - Web Developer Portfolio',
+  description: 'Showcasing the portfolio of DCG, a skilled web developer specializing in modern web technologies.',
+  keywords: 'web developer, portfolio, Diego, Didac, JavaScript, React, Next.js, dev, tech',
+  openGraph: {
+    title: 'DidacDev - Web Developer Portfolio',
+    description: 'Showcasing the portfolio of DidacDev, a skilled web developer specializing in modern web technologies.',
+    url: 'https://yourwebsite.com',
+    type: 'website',
+    images: [
+      {
+        url: 'https://yourwebsite.com/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'DCG Portfolio',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@yourtwitterhandle',
+    title: 'DCG - Web Developer Portfolio',
+    description: 'Showcasing the portfolio of DCG, a skilled web developer specializing in modern web technologies.',
+    image: 'https://yourwebsite.com/twitter-image.jpg',
+  },
+};
+
 
 export default function RootLayout({ children }) {
   return (
